36077bba8d7f1a165023dbf9d47a698660d2c89a
SuoRuGithub <z-z23@mails.tsinghua.edu.cn>
2024-10-02 19:16:54 +0800

    setup pipeline

--- userManager.sh
+++ userManager.sh
@@ -10,16 +10,26 @@ get_users() {
         -i inventory/inventory.yaml playbooks/get_users_info.yaml
 }
 
+# 每次添加 / 删除用户之前和之后都需要获取用户信息，尽可能保证本地存储的用户信息和远程主机上的用户信息一致
+
 # add function
 add_users() {
     echo "add users"
+    ansible-playbook --ask-become-pass --extra_vars "project_root=CURRENT_DIR" \
+        -i inventory/inventory.yaml playbooks/get_users_info.yaml
 
+    ansible-playbook --ask-become-pass --extra_vars "project_root=CURRENT_DIR" \
+        -i inventory/inventory.yaml playbooks/get_users_info.yaml
 }
 
 # delete function
 delete_users() {
     echo "delete users"
+    ansible-playbook --ask-become-pass --extra_vars "project_root=CURRENT_DIR" \
+        -i inventory/inventory.yaml playbooks/get_users_info.yaml
 
+    ansible-playbook --ask-become-pass --extra_vars "project_root=CURRENT_DIR" \
+        -i inventory/inventory.yaml playbooks/get_users_info.yaml
 }
 
 if [[ $# -eq 0 ]]; then
